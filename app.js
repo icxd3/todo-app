8223d692117fcdf90e0b9b6e649fbaa94a03f46b
Cxd3 <icxd3@proton.me>
2026-01-09 23:36:47 +0300

    add: function addTask was created

--- app.js
+++ app.js
@@ -3,5 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const addTaskBtn = document.getElementById('add-task-btn');
     const taskList = document.getElementById('task-list');
 
-    
-})
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.ariaValueMax.trim();
+        if(!taskText) {
+            return;
+        }
+
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    };
+
+    addTaskBtn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+            addTask(e);
+        }
+    });
+});
