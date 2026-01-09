e13b0d96d9b8cbb7a96897d67dbebcf56e89bf75
Cxd3 <icxd3@proton.me>
2026-01-09 23:26:13 +0300

    add: variables was created in app.js file

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('task-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+
+    
+})
